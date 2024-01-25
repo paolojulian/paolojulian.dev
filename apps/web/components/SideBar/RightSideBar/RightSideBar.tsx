7daf0a68e948d5d564f2544a563b100b3d18bf8e
Paolo Vincent Julian <paolo.julian@accelo.com>
2024-01-25 20:45:44 +0800

    style // Add sidebar hover effect

--- apps/web/components/SideBar/RightSideBar/RightSideBar.tsx
+++ apps/web/components/SideBar/RightSideBar/RightSideBar.tsx
@@ -1,23 +1,16 @@
 import Stack from '@repo/ui/components/stack';
 import Typography from '@repo/ui/components/typography';
+import classNames from 'classnames';
 import Link from 'next/link';
 
 export default function RightSideBar() {
   return (
     <Stack className='fixed right-10 top-10 bottom-10 justify-between items-end z-30'>
       <Stack className='gap-2 items-end'>
-        <Link href='#'>
-          <Typography className='text-gray'>ABOUT</Typography>
-        </Link>
-        <Link href='#'>
-          <Typography className='text-gray'>EXPERIENCE</Typography>
-        </Link>
-        <Link href='#'>
-          <Typography className='text-gray'>WRITING</Typography>
-        </Link>
-        <Link href='#'>
-          <Typography className='text-gray'>CONTACT</Typography>
-        </Link>
+        <SideBarLink isCurrent={true} href='#about' label='About' />
+        <SideBarLink isCurrent={false} href='#experience' label='Experience' />
+        <SideBarLink isCurrent={false} href='#writing' label='Writing' />
+        <SideBarLink isCurrent={false} href='#contact' label='Contact' />
       </Stack>
       <span
         style={{
@@ -29,5 +22,27 @@ export default function RightSideBar() {
         </Typography>
       </span>
     </Stack>
+  );
+}
+
+function SideBarLink({
+  href,
+  label,
+  isCurrent,
+}: {
+  href: string;
+  label: string;
+  isCurrent?: boolean;
+}) {
+  return (
+    <Link
+      href={href}
+      className={classNames('duration-500 ease-in-out', {
+        'text-white pointer-events-none': isCurrent,
+        'text-gray hover:text-primary hover:-translate-x-4': !isCurrent,
+      })}
+    >
+      <Typography className='uppercase'>{label}</Typography>
+    </Link>
   );
 }
