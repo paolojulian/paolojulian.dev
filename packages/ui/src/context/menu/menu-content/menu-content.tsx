e9253b52589e834a538530a7ac38ed9ec70099b5
Paolo Vincent Julian <paolo.julian@accelo.com>
2024-02-02 18:41:51 +0800

    feat // add menu items

--- packages/ui/src/context/menu/menu-content/menu-content.tsx
+++ packages/ui/src/context/menu/menu-content/menu-content.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { ReactNode } from 'react';
 import { useMenuContext } from '../context/menu-provider';
+import cn from '../../../utils/cn';
 
 interface Props {
   children: ReactNode;
@@ -17,21 +18,23 @@ export default function MenuContent({ children }: Props) {
 
   return (
     <div
-      className={[
-        'flex-1 motion-reduce:transition-none',
-        'ease-[cubic-bezier(0.45,0.02,0.09,0.98)]',
-        'duration-500 relative',
-        'bg-new-black',
-        'flex flex-col',
-        'h-auto w-screen overflow-x-hidden min-h-screen',
-        isOpen ? 'cursor-pointer overflow-hidden' : '',
-        isOpen
-          ? '-translate-y-[400px] md:-translate-y-[500px]'
-          : 'translate-y-0',
-      ].join(' ')}
+      className={cn(
+        [
+          'ui-flex-1 motion-reduce:ui-transition-none',
+          'ui-ease-[cubic-bezier(0.45,0.02,0.09,0.98)]',
+          'ui-duration-500 ui-relative',
+          'ui-bg-new-black',
+          'ui-flex ui-flex-col',
+          'ui-h-auto ui-w-screen ui-overflow-x-hidden ui-min-h-screen',
+          isOpen ? 'ui-cursor-pointer ui-overflow-hidden' : '',
+          isOpen
+            ? '-ui-translate-y-[400px] md:-ui-translate-y-[500px]'
+            : 'ui-translate-y-0',
+        ].join(' ')
+      )}
       onClick={handleClick}
     >
-      <div className={isOpen ? 'pointer-events-none' : ''}>{children}</div>
+      <div className={isOpen ? 'ui-pointer-events-none' : ''}>{children}</div>
     </div>
   );
 }
