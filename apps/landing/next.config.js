fce54fe171f32a8feadb73cf76627abf65d940bd
Paolo Vincent Julian <paolo.julian@accelo.com>
2024-02-04 20:02:26 +0800

    feat // add redirects to old page

--- apps/landing/next.config.js
+++ apps/landing/next.config.js
@@ -1,5 +1,24 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/about',
+        destination: 'https://v1.paolojulian.dev/about',
+        permanent: true,
+      },
+      {
+        source: '/blogs*',
+        destination: 'https://v1.paolojulian.dev/blogs*',
+        permanent: true,
+      },
+      {
+        source: '/apps*',
+        destination: 'https://v1.paolojulian.dev/apps*',
+        permanent: true,
+      },
+    ];
+  },
   reactStrictMode: true,
   transpilePackages: ["@repo/ui"],
   images: {
