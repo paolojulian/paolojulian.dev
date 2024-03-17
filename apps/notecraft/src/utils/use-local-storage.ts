1293f4c10eaeedd382232ce10fe7f65a43bbb510
Paolo Vincent Julian <paolo.julian@accelo.com>
2024-03-17 10:02:31 +0800

    feat // localStorage for server side

--- apps/notecraft/src/utils/use-local-storage.ts
+++ apps/notecraft/src/utils/use-local-storage.ts
@@ -1,3 +1,4 @@
+'use client';
 import { useState, useEffect, Dispatch, SetStateAction } from 'react';
 
 // Custom hook for persisting state in localStorage
@@ -6,7 +7,8 @@ function useLocalStorage<T>(
   initialValue: T
 ): [T, Dispatch<SetStateAction<T>>] {
   // Retrieve the stored value from localStorage (if available)
-  const storedValue = localStorage.getItem(key);
+  const storedValue =
+    typeof window !== 'undefined' ? localStorage.getItem(key) : '';
   // Parse the stored value or use the initial value if it doesn't exist
   const initial = storedValue ? JSON.parse(storedValue) : initialValue;
   // Create state to store the value
@@ -14,7 +16,9 @@ function useLocalStorage<T>(
 
   // Update localStorage whenever the state changes
   useEffect(() => {
-    localStorage.setItem(key, JSON.stringify(value));
+    if (typeof window !== 'undefined') {
+      localStorage.setItem(key, JSON.stringify(value));
+    }
   }, [key, value]);
 
   // Return the value and a function to update it
