d834b2fff90119f85473d0fb86f0c097eead63d3
Paolo Vincent Julian <paolo.julian@accelo.com>
2024-03-20 16:53:05 +0800

    Add ear training result component and implement logic for displaying question and result

---
+++ apps/notecraft/src/components/workarea/ear-training-screen/components/ear-training-result.tsx
@@ -0,0 +1,60 @@
+'use client';
+
+import SuccessFailCard from '@/components/common/success-fail-card';
+import Typography from '@/components/common/typography/typography';
+import { Note } from '@/types/note-trainer.types';
+import cn from '@repo/ui/utils/cn';
+import { Fragment } from 'react';
+
+export default function EarTrainingResult({
+  onNext,
+  correctNote,
+  isCorrect,
+}: {
+  onNext: () => void;
+  correctNote: Note;
+  isCorrect: boolean;
+}) {
+  const answerText = isCorrect ? (
+    <Fragment>
+      <div>You</div>
+      <div>are</div>
+      <div>correct</div>
+    </Fragment>
+  ) : (
+    <Fragment>
+      <div>You</div>
+      <div>are</div>
+      <div>wrong</div>
+    </Fragment>
+  );
+
+  return (
+    <Fragment>
+      <SuccessFailCard
+        onClick={onNext}
+        title={answerText}
+        type={isCorrect ? 'success' : 'fail'}
+      >
+        <div
+          className={cn(
+            'aspect-square w-16',
+            'rounded-xl',
+            'flex items-center justify-center',
+            {
+              ['bg-green']: isCorrect,
+              ['bg-red']: !isCorrect,
+            }
+          )}
+        >
+          <Typography
+            className={cn('uppercase', 'text-black')}
+            variant='heading-sm'
+          >
+            {correctNote}
+          </Typography>
+        </div>
+      </SuccessFailCard>
+    </Fragment>
+  );
+}
--- apps/notecraft/src/components/workarea/ear-training-screen/ear-training-screen.tsx
+++ apps/notecraft/src/components/workarea/ear-training-screen/ear-training-screen.tsx
@@ -4,14 +4,20 @@ import TrainerLayout from '@/components/common/layouts/trainer.layout';
 import NoteChoices from '@/components/common/note-choices';
 import PlayPauseNote from '@/components/common/play-pause-note';
 import SelectScale from '@/components/common/select-scale/select-scale';
+import EarTrainingResult from '@/components/workarea/ear-training-screen/components/ear-training-result';
 import { Note, Tone } from '@/types/note-trainer.types';
 import { Scale, getMajorScaleNotes } from '@/types/scale.types';
 import {
   generateRandomNotePerScale,
   generateRandomTone,
 } from '@/utils/generate-random-note-per-scale';
+import PreloadNotes from '@/utils/preload-notes';
 import useLocalStorage from '@/utils/use-local-storage';
-import { useCallback, useState } from 'react';
+import { memo, useEffect, useState } from 'react';
+
+const MemoizedPreloadNotes = memo(PreloadNotes);
+
+type DisplayTypes = 'question' | 'result';
 
 const INITIAL_SCALE: Scale = 'C major';
 
@@ -25,38 +31,63 @@ export default function EarTrainingScreen() {
     generateRandomNotePerScale(selectedScale)
   );
   const [randomTone, setRandomTone] = useState<Tone>(generateRandomTone());
+  const [displayType, setDisplayType] = useState<DisplayTypes>('question');
+
   const handleSelectNote = (note: Note) => {
     setSelectedNotes(note);
+    setDisplayType('result');
   };
   const handleGenerateNotes = () => getMajorScaleNotes(selectedScale);
 
-  const generateNote = useCallback(() => {
+  const generateNewNoteAndTone = () => {
     const randomNote = generateRandomNotePerScale(selectedScale);
     const randomTone = generateRandomTone();
     setRandomNote(randomNote);
     setRandomTone(randomTone);
+  };
+
+  const handleNext = () => {
+    setDisplayType('question');
+    setSelectedNotes(undefined);
+    generateNewNoteAndTone();
+  };
+
+  useEffect(() => {
+    generateNewNoteAndTone();
   }, [selectedScale]);
 
   return (
     <TrainerLayout title='Ear Training'>
-      <>
-        <PlayPauseNote key={randomNote} note={randomNote} tone={randomTone} />
-        <div className='w-full'>
-          <SelectScale
-            initialScale={selectedScale}
-            onSelectScale={setSelectedScale}
-          />
-        </div>
-        <NoteChoices
-          key={selectedScale}
-          onSelectNote={handleSelectNote}
-          generateNotes={handleGenerateNotes}
-          gridType='7'
-          title={'Select Correct Note'}
-          selectedNotes={selectedNote ? [selectedNote] : undefined}
-          shouldShuffleNotes={true}
-        />
-      </>
+      <MemoizedPreloadNotes>
+        <>
+          <PlayPauseNote key={randomNote} note={randomNote} tone={randomTone} />
+          {displayType === 'question' && (
+            <>
+              <div className='w-full'>
+                <SelectScale
+                  initialScale={selectedScale}
+                  onSelectScale={setSelectedScale}
+                />
+              </div>
+              <NoteChoices
+                key={selectedScale}
+                onSelectNote={handleSelectNote}
+                generateNotes={handleGenerateNotes}
+                gridType='7'
+                title={'Select Correct Note'}
+                selectedNotes={selectedNote ? [selectedNote] : undefined}
+              />
+            </>
+          )}
+          {displayType === 'result' && (
+            <EarTrainingResult
+              correctNote={randomNote}
+              isCorrect={randomNote === selectedNote}
+              onNext={handleNext}
+            />
+          )}
+        </>
+      </MemoizedPreloadNotes>
     </TrainerLayout>
   );
 }
