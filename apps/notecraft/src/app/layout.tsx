2a0c02ae6791cf4dbb06658982127571e2e88306
Paolo Vincent Julian <paolo.julian@accelo.com>
2024-04-04 17:09:52 +0800

    Add viewport meta tag and preload audio files

--- apps/notecraft/src/app/layout.tsx
+++ apps/notecraft/src/app/layout.tsx
@@ -17,6 +17,10 @@ export default function RootLayout({
       <head>
         <link rel='manifest' href='/manifest.json' />
         <meta name='theme-color' content='#0d0d0d' />
+        <meta
+          name='viewport'
+          content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover'
+        />
         <link
           rel='apple-touch-icon'
           sizes='180x180'
@@ -36,44 +40,44 @@ export default function RootLayout({
         />
         <link rel='mask-icon' href='/safari-pinned-tab.svg' color='#fcf5ed' />
 
-        <link rel="preload" href="/notes/C-low.mp3" as="audio" />
-        <link rel="preload" href="/notes/C#-low.mp3" as="audio" />
-        <link rel="preload" href="/notes/D-low.mp3" as="audio" />
-        <link rel="preload" href="/notes/D#-low.mp3" as="audio" />
-        <link rel="preload" href="/notes/E-low.mp3" as="audio" />
-        <link rel="preload" href="/notes/F-low.mp3" as="audio" />
-        <link rel="preload" href="/notes/F#-low.mp3" as="audio" />
-        <link rel="preload" href="/notes/G-low.mp3" as="audio" />
-        <link rel="preload" href="/notes/G#-low.mp3" as="audio" />
-        <link rel="preload" href="/notes/A-low.mp3" as="audio" />
-        <link rel="preload" href="/notes/A#-low.mp3" as="audio" />
-        <link rel="preload" href="/notes/B-low.mp3" as="audio" />
+        <link rel='preload' href='/notes/C-low.mp3' as='audio' />
+        <link rel='preload' href='/notes/C#-low.mp3' as='audio' />
+        <link rel='preload' href='/notes/D-low.mp3' as='audio' />
+        <link rel='preload' href='/notes/D#-low.mp3' as='audio' />
+        <link rel='preload' href='/notes/E-low.mp3' as='audio' />
+        <link rel='preload' href='/notes/F-low.mp3' as='audio' />
+        <link rel='preload' href='/notes/F#-low.mp3' as='audio' />
+        <link rel='preload' href='/notes/G-low.mp3' as='audio' />
+        <link rel='preload' href='/notes/G#-low.mp3' as='audio' />
+        <link rel='preload' href='/notes/A-low.mp3' as='audio' />
+        <link rel='preload' href='/notes/A#-low.mp3' as='audio' />
+        <link rel='preload' href='/notes/B-low.mp3' as='audio' />
 
-        <link rel="preload" href="/notes/C-mid.mp3" as="audio" />
-        <link rel="preload" href="/notes/C#-mid.mp3" as="audio" />
-        <link rel="preload" href="/notes/D-mid.mp3" as="audio" />
-        <link rel="preload" href="/notes/D#-mid.mp3" as="audio" />
-        <link rel="preload" href="/notes/E-mid.mp3" as="audio" />
-        <link rel="preload" href="/notes/F-mid.mp3" as="audio" />
-        <link rel="preload" href="/notes/F#-mid.mp3" as="audio" />
-        <link rel="preload" href="/notes/G-mid.mp3" as="audio" />
-        <link rel="preload" href="/notes/G#-mid.mp3" as="audio" />
-        <link rel="preload" href="/notes/A-mid.mp3" as="audio" />
-        <link rel="preload" href="/notes/A#-mid.mp3" as="audio" />
-        <link rel="preload" href="/notes/B-mid.mp3" as="audio" />
+        <link rel='preload' href='/notes/C-mid.mp3' as='audio' />
+        <link rel='preload' href='/notes/C#-mid.mp3' as='audio' />
+        <link rel='preload' href='/notes/D-mid.mp3' as='audio' />
+        <link rel='preload' href='/notes/D#-mid.mp3' as='audio' />
+        <link rel='preload' href='/notes/E-mid.mp3' as='audio' />
+        <link rel='preload' href='/notes/F-mid.mp3' as='audio' />
+        <link rel='preload' href='/notes/F#-mid.mp3' as='audio' />
+        <link rel='preload' href='/notes/G-mid.mp3' as='audio' />
+        <link rel='preload' href='/notes/G#-mid.mp3' as='audio' />
+        <link rel='preload' href='/notes/A-mid.mp3' as='audio' />
+        <link rel='preload' href='/notes/A#-mid.mp3' as='audio' />
+        <link rel='preload' href='/notes/B-mid.mp3' as='audio' />
 
-        <link rel="preload" href="/notes/C-high.mp3" as="audio" />
-        <link rel="preload" href="/notes/C#-high.mp3" as="audio" />
-        <link rel="preload" href="/notes/D-high.mp3" as="audio" />
-        <link rel="preload" href="/notes/D#-high.mp3" as="audio" />
-        <link rel="preload" href="/notes/E-high.mp3" as="audio" />
-        <link rel="preload" href="/notes/F-high.mp3" as="audio" />
-        <link rel="preload" href="/notes/F#-high.mp3" as="audio" />
-        <link rel="preload" href="/notes/G-high.mp3" as="audio" />
-        <link rel="preload" href="/notes/G#-high.mp3" as="audio" />
-        <link rel="preload" href="/notes/A-high.mp3" as="audio" />
-        <link rel="preload" href="/notes/A#-high.mp3" as="audio" />
-        <link rel="preload" href="/notes/B-high.mp3" as="audio" />
+        <link rel='preload' href='/notes/C-high.mp3' as='audio' />
+        <link rel='preload' href='/notes/C#-high.mp3' as='audio' />
+        <link rel='preload' href='/notes/D-high.mp3' as='audio' />
+        <link rel='preload' href='/notes/D#-high.mp3' as='audio' />
+        <link rel='preload' href='/notes/E-high.mp3' as='audio' />
+        <link rel='preload' href='/notes/F-high.mp3' as='audio' />
+        <link rel='preload' href='/notes/F#-high.mp3' as='audio' />
+        <link rel='preload' href='/notes/G-high.mp3' as='audio' />
+        <link rel='preload' href='/notes/G#-high.mp3' as='audio' />
+        <link rel='preload' href='/notes/A-high.mp3' as='audio' />
+        <link rel='preload' href='/notes/A#-high.mp3' as='audio' />
+        <link rel='preload' href='/notes/B-high.mp3' as='audio' />
 
         <meta name='msapplication-TileColor' content='#b91d47' />
         <meta name='theme-color' content='#fcf5ed' />
