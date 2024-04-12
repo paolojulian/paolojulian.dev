f6456d277e5c42ee7a7eff6b47fc890069ab5637
Paolo Vincent Julian <paolo.julian@accelo.com>
2024-04-12 22:19:01 +0800

    Add link to logo in LeftSideBar component

--- apps/about/src/components/side-bar/left-side-bar/left-side-bar.tsx
+++ apps/about/src/components/side-bar/left-side-bar/left-side-bar.tsx
@@ -2,6 +2,7 @@ import Stack from '@repo/ui/components/stack';
 import LinkedInIcon from '@repo/ui/icons/linkedin-icon';
 import MailIcon from '@repo/ui/icons/mail-icon';
 import PhoneIcon from '@repo/ui/icons/phone-icon';
+import links from '@repo/ui/utils/links';
 import Image from 'next/image';
 import Link from 'next/link';
 import { ReactElement } from 'react';
@@ -9,9 +10,9 @@ import { ReactElement } from 'react';
 export default function LeftSideBar() {
   return (
     <Stack className='fixed left-4 md:left-10 top-6 md:top-10 bottom-6 md:bottom-10 justify-between items-center z-30'>
-      <Link className='w-8 2xl:w-10 aspect-square relative' href='/'>
+      <a className='w-8 2xl:w-10 aspect-square relative' href={links.base}>
         <Image alt='Logo' src='/logo.png' fill quality={100} />
-      </Link>
+      </a>
     </Stack>
   );
 }
