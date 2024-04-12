370056b0ba4eee6d240fde44db858f5804497837
Paolo Vincent Julian <paolo.julian@accelo.com>
2024-04-12 17:12:18 +0800

    Initialize about page

---
+++ apps/about/next.config.mjs
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  transpilePackages: ["@repo/ui"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.ctfassets.net',
+        port: '',
+      },
+      {
+        protocol: 'https',
+        hostname: 'assets.ctfassets.net',
+        port: '',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
