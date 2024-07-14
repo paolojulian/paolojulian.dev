1f0dd431eab2aa9e52cff0f90d7cf9ff82554dbf
Paolo Vincent Julian <paolo.julian@accelo.com>
2024-07-14 13:15:15 +0800

    feat: add app-item component

--- apps/landing/app/apps/_app-list.tsx
+++ apps/landing/app/apps/_app-list.tsx
@@ -1,10 +1,36 @@
 import SectionHeader from '@repo/ui/components/SectionHeader';
 import Stack from '@repo/ui/components/stack';
+import AppItem from './components/app-item/app-item';
+
+type AppItemFields = {
+  id: string;
+  title: string;
+  description: string;
+  imageURL: string;
+  link: string;
+  type: string;
+};
+const mockApps: AppItemFields[] = [
+  {
+    id: '1',
+    description:
+      'Git Branch Updater is a script to aid merging and updating multiple dependent branch at once.',
+    imageURL: '/images/git-branch-updater.png',
+    link: '#',
+    title: 'Git Branch Updater',
+    type: 'Script',
+  },
+];
 
 export default function AppList() {
   return (
     <Stack className='my-20'>
       <SectionHeader title='Apps' />
+      <div className='grid grid-cols-3'>
+        {mockApps.map(({ id, ...app }) => (
+          <AppItem key={id} {...app} />
+        ))}
+      </div>
     </Stack>
   );
 }
---
+++ apps/landing/app/apps/components/app-item/app-item.tsx
@@ -0,0 +1,55 @@
+import Stack from "@repo/ui/components/stack";
+import Typography from "@repo/ui/components/typography";
+import Image from "next/image";
+
+export type AppItemProps = {
+  description: string;
+  link: string;
+  title: string;
+  type: string;
+  imageURL: string;
+};
+
+export default function AppItem({
+  description,
+  link,
+  imageURL,
+  title,
+  type,
+}: AppItemProps) {
+  return (
+    <a href={link} className="cursor-pointer">
+      <div className="flex flex-col gap-6 group">
+        {/* Image */}
+        <div className="relative aspect-[385/256] w-full bg-white rounded-md border-4 border-gray overflow-hidden">
+          <Image alt={title} fill src={imageURL} />
+          <div className="absolute inset-0 -translate-x-full group-hover:translate-x-0 duration-500 ease-in-out bg-primary/50"></div>
+        </div>
+
+        {/* Header */}
+        <Stack className="flex-1 justify-center items-start gap-2">
+          <Typography
+            className="text-gray-darker uppercase"
+            variant="body-wide"
+          >
+            {type}
+          </Typography>
+          <Typography
+            className="line-clamp-3 md:line-clamp-2 text-white group-hover:text-primary duration-500 ease-in-out"
+            variant="heading"
+          >
+            {title}
+          </Typography>
+        </Stack>
+
+        {/* Description */}
+        <Typography
+          className="line-clamp-3 md:line-clamp-2 text-white group-hover:text-primary duration-500 ease-in-out"
+          variant="body"
+        >
+          {description}
+        </Typography>
+      </div>
+    </a>
+  );
+}
