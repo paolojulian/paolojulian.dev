067df4f2da8d6c54023d229c99ce869047cabb79
Paolo Vincent Julian <paolo.julian@accelo.com>
2024-07-14 21:57:19 +0800

    style: responsive app list

--- apps/landing/app/apps/_app-list.tsx
+++ apps/landing/app/apps/_app-list.tsx
@@ -18,7 +18,7 @@ export default async function AppList() {
       </Stack>
       <div className='border-t border-white w-full'></div>
 
-      <div className='grid grid-cols-3 gap-6'>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
         {appItems.map((appItem) => (
           <AppItem key={appItem.sys.id} appItem={appItem} />
         ))}
