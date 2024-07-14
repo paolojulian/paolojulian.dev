c0ecf7b67eb8bc4770a3ec186cd14dbe24ed8162
Paolo Vincent Julian <paolo.julian@accelo.com>
2024-07-14 14:31:11 +0800

    feat: integrate app from collection

--- apps/landing/app/apps/_app-list.tsx
+++ apps/landing/app/apps/_app-list.tsx
@@ -2,47 +2,13 @@ import SectionHeader from '@repo/ui/components/SectionHeader';
 import Stack from '@repo/ui/components/stack';
 import AppItem from './components/app-item/app-item';
 import Typography from '@repo/ui/components/typography';
+import { useAppItems } from './graphql/use-app-items';
 
-type AppItemFields = {
-  id: string;
-  title: string;
-  description: string;
-  imageURL: string;
-  link: string;
-  type: string;
-};
+export default async function AppList() {
+  const appItems = await useAppItems();
 
-const mockApps: AppItemFields[] = [
-  {
-    id: '1',
-    description:
-      'Git Branch Updater is a script to aid merging and updating multiple dependent branch at once.',
-    imageURL: '/images/git-branch-updater.png',
-    link: '#',
-    title: 'Git Branch Updater',
-    type: 'Script',
-  },
-  {
-    id: '2',
-    description:
-      'My Day App is a budget tracking and task tracking help to track monthly expenses, tasks and able to view statistics of monthly expenses.',
-    imageURL: '/images/git-branch-updater.png',
-    link: '#',
-    title: 'My Day App',
-    type: 'Mobile',
-  },
-  {
-    id: '3',
-    description:
-      'A pomodoro with a twist, the pomodoro also has a timer going up whenever the pomodoro is on WORK mode, this helps people that their company has log time.',
-    imageURL: '/images/git-branch-updater.png',
-    link: '#',
-    title: 'Pomodoro w/ timer',
-    type: 'Web app',
-  },
-];
+  console.log('test', { appItems });
 
-export default function AppList() {
   return (
     <Stack className='my-20 gap-10'>
       <Stack className='gap-10'>
@@ -53,9 +19,10 @@ export default function AppList() {
         </Typography>
       </Stack>
       <div className='border-t border-white w-full'></div>
+
       <div className='grid grid-cols-3 gap-6'>
-        {mockApps.map(({ id, ...app }) => (
-          <AppItem key={id} {...app} />
+        {appItems.map((appItem) => (
+          <AppItem key={appItem.sys.id} appItem={appItem} />
         ))}
       </div>
     </Stack>
--- apps/landing/app/apps/components/app-item/app-item.tsx
+++ apps/landing/app/apps/components/app-item/app-item.tsx
@@ -1,28 +1,21 @@
 import Stack from '@repo/ui/components/stack';
 import Typography from '@repo/ui/components/typography';
 import Image from 'next/image';
+import { type AppItemFields } from '../../graphql/use-app-items';
 
 export type AppItemProps = {
-  description: string;
-  link: string;
-  title: string;
-  type: string;
-  imageURL: string;
+  appItem: AppItemFields;
 };
 
-export default function AppItem({
-  description,
-  link,
-  imageURL,
-  title,
-  type,
-}: AppItemProps) {
+export default function AppItem({ appItem }: AppItemProps) {
+  const { title, description, banner, link, type } = appItem;
+
   return (
     <a href={link} className='cursor-pointer'>
       <div className='flex flex-col gap-6 group'>
         {/* Image */}
         <div className='relative aspect-[385/256] w-full bg-white rounded-md border-4 border-gray overflow-hidden'>
-          <Image alt={title} fill src={imageURL} />
+          <Image alt={banner.title} fill src={banner.url} />
           <div className='absolute inset-0 -translate-x-full group-hover:translate-x-0 duration-500 ease-in-out bg-primary/50'></div>
         </div>
 
@@ -35,7 +28,7 @@ export default function AppItem({
             {type}
           </Typography>
           <Typography
-            className='line-clamp-3 md:line-clamp-2 text-white group-hover:text-primary duration-500 ease-in-out'
+            className='line-clamp-2 text-white group-hover:text-primary duration-500 ease-in-out'
             variant='heading'
           >
             {title}
@@ -44,7 +37,7 @@ export default function AppItem({
 
         {/* Description */}
         <Typography
-          className='line-clamp-3 md:line-clamp-2 text-white group-hover:text-primary duration-500 ease-in-out'
+          className='line-clamp-4 text-white group-hover:text-primary duration-500 ease-in-out'
           variant='body'
         >
           {description}
